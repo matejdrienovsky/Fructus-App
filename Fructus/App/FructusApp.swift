d9514f1419e32de7b414a22917978b2903f8c281
Matej Drienovský <matejdrienovsky123@gmail.com>
2023-08-28 22:19:27 +0200

    first

---
+++ Fructus/App/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Ivo Pecivko on 17/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+        }
+    }
+}
---
+++ Fructus/Utility/Colors.swift
@@ -0,0 +1,38 @@
+//
+//  Colors.swift
+//  Fructus
+//
+//  Created by Ivo Pecivko on 17/08/2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let blueberryLight = Color("ColorBlueberryLight")
+    static let blueberryDark = Color("ColorBlueberryDark")
+    static let appleLight = Color("ColorAppleLight")
+    static let appleDark = Color("ColorAppleDark")
+    static let cherryLight = Color("ColorCherryLight")
+    static let cherryDark = Color("ColorCherryDark")
+    static let gooseberryDark = Color("ColorGooseberryDark")
+    static let gooseberryLight = Color("ColorGooseberryLight")
+    static let grapefruitDark = Color("ColorGrapefruitDark")
+    static let grapefruitLight = Color("ColorGrapefruitLight")
+    static let lemonDark = Color("ColorLemonDark")
+    static let lemonLight = Color("ColorLemonLight")
+    static let limeDark = Color("ColorLimeDark")
+    static let limeLight = Color("ColorLimeLight")
+    static let mangoDark = Color("ColorMangoDark")
+    static let mangoLight = Color("ColorMangoLight")
+    static let pearDark = Color("ColorPearDark")
+    static let pearLight = Color("ColorPearLight")
+    static let plumDark = Color("ColorPlumDark")
+    static let plumLight = Color("ColorPlumLight")
+    static let pomegranateDark = Color("ColorPomegranateDark")
+    static let pomegranateLight = Color("ColorPomegranateLight")
+    static let strawberryDark = Color("ColorStrawberryDark")
+    static let strawberryLight = Color("ColorStrawberryLight")
+    static let watermelonDark = Color("ColorWatermelonDark")
+    static let watermelonLight = Color("ColorWatermelonLight")
+}
